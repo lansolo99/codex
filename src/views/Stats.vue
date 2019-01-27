b3ff0180494c4ee9d070b2658cdb9d4fa48311d2
lansolo99 <lansolo99@hotmail.fr>
2019-01-27 19:19:57 +0200

    Try Apexchart Vue cpnt

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -32,37 +32,217 @@
           padding="30"
           auto-draw
         ></v-sparkline>
+
+        <div class="example">
+          <apexcharts
+            width="100%"
+            height="300"
+            type="heatmap"
+            :options="chartOptions"
+            :series="series"
+          ></apexcharts>
+        </div>
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {
-  data: () => ({
+import VueApexCharts from 'vue-apexcharts'
+import Vue from 'vue'
+Vue.use(VueApexCharts)
 
-    gradients: [
-      ['red', 'orange', 'yellow']
-    ],
-    labels: [
-      'Mon',
-      'Tue',
-      'Wed',
-      'Thu',
-      'Fri',
-      'Sat',
-      'Sun'
-    ],
-    value: [
-      100,
-      100,
-      100,
-      100,
-      100,
-      90,
-      100
-    ]
-  })
+export default {
+  components: {
+    apexcharts: VueApexCharts
+  },
+  data () {
+    return {
+
+      chartOptions: {
+        chart: {
+          toolbar: {
+            show: false
+          },
+          animations: {
+            enabled: false,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+              enabled: false,
+              delay: 150
+            },
+            dynamicAnimation: {
+              enabled: false,
+              speed: 350
+            }
+          }
+        },
+
+        dataLabels: {
+          enabled: false
+        },
+        colors: ['#F44336'],
+        tooltip: {
+          enabled: false
+        },
+        xaxis: {
+          type: 'category',
+          position: 'bottom',
+          categories: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+          offsetY: '-250',
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          }
+        },
+        title: {
+          text: ''
+        }
+
+      },
+      series: [{
+        name: 'W10',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W9',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W8',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W7',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W6',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W5',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W4',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W3',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W2',
+        data: this.generateData(7, {
+          min: 0,
+          max: 90
+        })
+      },
+      {
+        name: 'W1',
+        data: [{
+          x: 'W1',
+          y: 22
+        }, {
+          x: 'W2',
+          y: 29
+        }, {
+          x: 'W3',
+          y: 13
+        }, {
+          x: 'W4',
+          y: 32
+        },
+        {
+          x: 'W4',
+          y: 32
+        },
+        {
+          x: 'W4',
+          y: 50
+        },
+        {
+          x: 'W4',
+          y: 100
+        }
+
+        ]
+        // data: this.generateData(7, {
+        //   min: 0,
+        //   max: 90
+        // })
+      }
+      ],
+
+      gradients: [
+        ['red', 'orange', 'yellow']
+      ],
+      labels: [
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thu',
+        'Fri',
+        'Sat',
+        'Sun'
+      ],
+      value: [
+        100,
+        100,
+        100,
+        100,
+        100,
+        90,
+        100
+      ]
+    }
+  },
+  methods: {
+    generateData (count, yrange) {
+      var i = 0
+      var series = []
+      while (i < count) {
+        var x = (i + 1).toString()
+        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+
+        series.push({
+          x: x,
+          y: y
+        })
+        i++
+      }
+      return series
+    }
+  }
+
 }
 </script>
 
