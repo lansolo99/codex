4e07067dff06bbe380cb3cb256a5ebaeb1489f8e
lansolo99 <lansolo99@hotmail.fr>
2019-02-07 21:13:16 +0200

    WIPTIME#6 - drop progressToday feature

--- src/components/TasksProgress.vue
+++ src/components/TasksProgress.vue
@@ -1,36 +1,13 @@
 <template>
   <v-container class="taskProgress pt-0 pb-2">
     <v-layout>
-      <v-flex xs6>
-        <v-card
-          flat
-          height="100%"
-          class="taskProgressContainer taskProgressContainer--bars transparent"
-        >
-          <span
-            class="label white--text"
-          >TODAY ({{this.userData.stats.dailyTasksChecked}}/{{this.userData.stats.dailyTasks.length}} tasks)</span>
-
-          <div class="progressbarContainer">
-            <v-progress-linear
-              v-model="this.userData.stats.progressToday"
-              height="15"
-              class="mt-2"
-              width="80%"
-            ></v-progress-linear>
-            <div
-              class="progressbarContainer__value white--text text-xs-right"
-            >{{this.userData.stats.progressToday}}%</div>
-          </div>
-        </v-card>
-      </v-flex>
-      <v-flex xs6>
+      <v-flex xs12>
         <v-card
           flat
           height="100%"
           class="taskProgressContainer taskProgressContainer--bars taskProgressContainer--week transparent"
         >
-          <span class="label white--text">THIS WEEK (day {{time.isoDay}}/7)</span>
+          <span class="label white--text">THIS WEEK GLOBAL SCORE (day {{time.isoDay}}/7)</span>
           <div class="progressbarContainer">
             <v-progress-linear
               v-model="this.userData.stats.progressWeek"
@@ -79,71 +56,12 @@ export default {
       return this.tasks
     }
   },
-  watch: {
-    tasksChecked: {
-      handler: function (val, oldVal) {
-        // EventBus.$emit('recordProgress')
-      },
-      deep: true
-    }
-  },
   methods: {
     ...mapActions({
-      recordProgress: 'profile/recordProgress',
-      recordWeekScore: 'profile/recordWeekScore',
       updateTime: 'time/updateTime',
+      recordWeekScore: 'profile/recordWeekScore',
       updateProfile: 'profile/updateProfile'
     }),
-    calcDailyCompletion () {
-      // todayProgress value is based on everydays + all specific days + singles
-
-      // Filter daily tasks from tasks
-      const dailyTasks = Object.values(this.tasks)
-        .filter(task => {
-          return (task.schedule.periodicity === 'Weekly' &&
-        task.schedule.weekly === 'Everyday') ||
-        (task.schedule.periodicity === 'On specific days' &&
-        task.schedule.specificDays.find(v => { return v === getStringFromIsoDay(this.time.isoDay) })) ||
-        (task.schedule.periodicity === 'Once' &&
-        task.schedule.once === 'single')
-        })
-
-      // UI feed
-      this.userData.stats.dailyTasks = dailyTasks
-
-      // Distribute tasks value
-
-      const taskValue = 100 / dailyTasks.length
-      let total = 0
-      let totalSubtasks = 0
-
-      // Count checked
-      const countCheckedTasks = Object.entries(dailyTasks)
-        .filter(v => { return v[1].checked === true })
-        .length
-
-      // UI feed
-      this.userData.stats.dailyTasksChecked = countCheckedTasks
-
-      // Distribute subtasks values
-      Object.entries(dailyTasks)
-        .filter(v => { return v[1].checked === false && v[1].subtasks.length > 0 })
-        .forEach(v => {
-          const subtaskLength = v[1].subtasks.length
-          const subtaskValue = taskValue / subtaskLength
-
-          const countSubtasksChecked = Object.entries(v[1].subtasks)
-            .filter(sub => { return sub[1].checked === true })
-            .length
-          totalSubtasks += subtaskValue * countSubtasksChecked
-        })
-
-      // Set todayProgress value
-      total = Math.trunc((taskValue * countCheckedTasks) + totalSubtasks)
-      if (isNaN(total)) { total = 0 }
-      this.progressToday = total
-    },
-
     calcWeeklyCompletion () {
       // Filter weekly tasks from tasks
       const weeklyTasks = Object.values(this.tasks)
@@ -177,7 +95,10 @@ export default {
       // Set weekProgress value
       total = Math.trunc(totalCompletions)
       if (isNaN(total)) { total = 0 }
-      this.progressWeek = total
+      // Update store
+      const progressWeek = total
+      const isoWeek = this.time.isoWeek
+      this.recordWeekScore({ progressWeek, isoWeek })
     }
 
   },
@@ -192,15 +113,7 @@ export default {
       this.updateTime({ isoDay, isoWeek })
 
       // Do calculations
-      this.calcDailyCompletion()
       this.calcWeeklyCompletion()
-
-      // Update store
-      const progressToday = this.userData.stats.progressToday
-      const progressWeek = this.userData.stats.progressWeek
-
-      this.recordProgress({ progressToday, isoDay })
-      this.recordWeekScore({ progressWeek, isoWeek })
     })
   }
 }
