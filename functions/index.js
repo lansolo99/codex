9ea4acc7c4899b3d7fe0e0d724bd9c14fc8bcc69
lan <lansolo99@hotmail.fr>
2019-04-01 21:21:36 +0200

    Implement push notification #14

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,26 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-// admin.initializeApp(functions.config().firebase)
-admin.initializeApp()
+admin.initializeApp(functions.config().firebase)
+//admin.initializeApp()
 
 // Send Notification
-exports.sendNotifications = functions.https.onCall((data, context) => {
+exports.sendNotifications = functions.firestore
+  .document('users/{userID}')
+  .onWrite((change, context) => {
+    //exports.sendNotifications = functions.https.onCall((data, context) => {
+    console.info(change);
 
-  const payload = {
-    notification: {
-      title: 'Notification title',
-      body: 'Notification info',
-      icon: 'http://i.imgur.com/image.png',
-      click_action: 'https://weekx.netlify.com'
+    const payload = {
+      notification: {
+        title: 'Notification title',
+        body: 'Notification info',
+        icon: 'http://i.imgur.com/image.png',
+        click_action: 'https://weekx.netlify.com'
+      }
     }
-  }
 
-  console.info(payload)
+    console.info(payload)
+    return admin.messaging().sendToDevice('fedcKkCXKns:APA91bHhX5GfHc_slAJCgIhkt7qetYprdoEI3vm2pqrX5VvmOByUz0CKuxllHCjV-pcBhNqmjEXzZXEVff9kd9KwwEBisYdA0NPDWPWOGRONESPUnvqkbEk3jY8S0wcjh71lrURpQRzO', payload)
+      .then((response) => console.info(response))
 
-  return admin.messaging().sendToDevice(data, payload)
-
-})
+  })
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,16 +5,6 @@
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-firebase.initializeApp({
-  //   'messagingSenderId': process.env.VUE_APP_MESSAGING_ID // 4. Get Firebase Configuration
-  // 'messagingSenderId': '545270095530'
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_ID
-})
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging()
