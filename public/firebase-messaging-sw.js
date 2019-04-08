470b08b06995c7e93aae513d7de9837abe40ce4e
lan <lansolo99@hotmail.fr>
2019-04-08 09:02:27 +0200

    Add badge notification #2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -27,7 +27,8 @@ messaging.setBackgroundMessageHandler(function (payload) {
   var notificationOptions = {
     body: payload.data.body,
     icon: 'https://weekx.netlify.com/img/icons/android-chrome-512x512.png',
-    click_action: 'https://weekx.netlify.com'
+    click_action: 'https://weekx.netlify.com',
+    badge: 'https://weekx.netlify.com/img/icons/notification-badge.png'
   }
 
   return self.registration.showNotification(notificationTitle,
