c041cfca688a3bd282736236da6ef1b6ca02523e
lan <lansolo99@hotmail.fr>
2019-05-14 18:26:44 +0200

    Fix domain links in service worker

--- functions/index.js
+++ functions/index.js
@@ -1,132 +1,149 @@
 // Imports
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const getHours = require('date-fns/get_hours')
-const setHours = require('date-fns/set_hours')
-const differenceInSeconds = require('date-fns/difference_in_seconds')
-const endOfToday = require('date-fns/end_of_today')
-const getISODay = require('date-fns/get_iso_day')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const getHours = require("date-fns/get_hours");
+const setHours = require("date-fns/set_hours");
+const differenceInSeconds = require("date-fns/difference_in_seconds");
+const endOfToday = require("date-fns/end_of_today");
+const getISODay = require("date-fns/get_iso_day");
 const {
   convertToLocalTime
-} = require('date-fns-timezone/dist/convertToLocalTime')
+} = require("date-fns-timezone/dist/convertToLocalTime");
 
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp(functions.config().firebase);
 
 // Utils
 const isoToWeekDays = {
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday',
-  7: 'Sunday'
-}
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+  7: "Sunday"
+};
 const getStringFromIsoDay = isoDay => {
-  return isoToWeekDays[isoDay]
-}
+  return isoToWeekDays[isoDay];
+};
 
 // Send notifications reminder
-exports.sendNotificationsReminder = functions.pubsub.topic('weekx-reminders').onPublish((message) => {
-  console.info(message)
+exports.sendNotificationsReminder = functions.pubsub
+  .topic("weekx-reminders")
+  .onPublish(message => {
+    console.info(message);
 
-  // Filter notification elligible users test
-  return admin.firestore()
-    .collection('users')
-    .get()
-    .then(users => {
-
-      /*=============================================
+    // Filter notification elligible users test
+    return admin
+      .firestore()
+      .collection("users")
+      .get()
+      .then(users => {
+        /*=============================================
                           LOOP
       =============================================*/
 
-      users.forEach(doc => {
-        // If user has a non-empty token
-        if (doc.data().profile.notifications.token && doc.data().profile.notifications.token !== '') {
-          console.info('user has token')
+        users.forEach(doc => {
+          // If user has a non-empty token
+          if (
+            doc.data().profile.notifications.token &&
+            doc.data().profile.notifications.token !== ""
+          ) {
+            console.info("user has token");
 
-          // If user has tasks
-          if (doc.data().tasks) {
-            console.info('user has tasks')
+            // If user has tasks
+            if (doc.data().tasks) {
+              console.info("user has tasks");
 
-            const serverHour = getHours(new Date(Date.now()))
-            const userDefinedHour = doc.data().profile.notifications.dailyTaskReminder.time
-            const userPushTTL = Math.abs(differenceInSeconds(new Date(Date.now()), endOfToday())).toString()
-            const userDate = setHours(new Date(Date.now()), userDefinedHour)
-            const userTimeZone = doc.data().profile.notifications.timezone
-            const userConvertedDate = convertToLocalTime(userDate, {
-              timeZone: userTimeZone
-            })
-            const userConvertedToServerHour = getHours(userConvertedDate)
-            console.log('serverHour = ' + serverHour)
-            console.log('userConvertedToServerHour = ' + userConvertedToServerHour)
+              const serverHour = getHours(new Date(Date.now()));
+              const userDefinedHour = doc.data().profile.notifications
+                .dailyTaskReminder.time;
+              const userPushTTL = Math.abs(
+                differenceInSeconds(new Date(Date.now()), endOfToday())
+              ).toString();
+              const userDate = setHours(new Date(Date.now()), userDefinedHour);
+              const userTimeZone = doc.data().profile.notifications.timezone;
+              const userConvertedDate = convertToLocalTime(userDate, {
+                timeZone: userTimeZone
+              });
+              const userConvertedToServerHour = getHours(userConvertedDate);
+              console.log("serverHour = " + serverHour);
+              console.log(
+                "userConvertedToServerHour = " + userConvertedToServerHour
+              );
 
-            if (serverHour === userConvertedToServerHour) {
-              console.info(`a user has defined an hour that match the current one which is ${serverHour}`)
-            }
-            // End hour test
-
-            // User defined hour is the right one
-            if (serverHour === userConvertedToServerHour) {
-              const dailyTasks = Object.keys(doc.data().tasks)
-                .map(e => doc.data().tasks[e])
-                .filter(task => {
-                  return (task.schedule.periodicity === 'Weekly' &&
-                      task.schedule.weekly === 'Everyday') ||
-                    (task.schedule.periodicity === 'On specific days' &&
-                      task.schedule.specificDays.find(v => {
-                        return v === getStringFromIsoDay(getISODay(Date.now()))
-                      })) ||
-                    (task.schedule.periodicity === 'Once' &&
-                      task.schedule.once === 'single')
-                })
-                .filter(task => {
-                  return task.checked === false
-                })
-
-              if (dailyTasks.length) {
-                // Send notification
-                console.log('user has daily tasks')
-                var plural = ""
-                var countDailyTasks = dailyTasks.length
-                if (dailyTasks.length > 1) {
-                  plural = "s"
-                } else {
-                  plural = ""
-                }
-
-                // Admin SDK
-                let message = {
-                  data: {
-                    title: 'Weekx',
-                    body: `You have ${countDailyTasks} task${plural} today!'`
-                  },
-                  token: doc.data().profile.notifications.token,
-                  "webpush": {
-                    "headers": {
-                      "TTL": userPushTTL
-                    },
-                    "notification": {
-                      "title": "Weekx",
-                      "body": `You have ${countDailyTasks} task${plural} today!'`,
-                      "icon": 'https://weekx.netlify.com/img/icons/android-chrome-512x512.png',
-                      "click_action": "https://weekx.netlify.com",
-                      "badge": 'https://weekx.netlify.com/img/icons/notification-badge.png'
-                    },
-                    "fcm_options": {
-                      "link": "https://weekx.netlify.com"
-                    }
-                  }
-                }
-                admin.messaging().send(message)
-
+              if (serverHour === userConvertedToServerHour) {
+                console.info(
+                  `a user has defined an hour that match the current one which is ${serverHour}`
+                );
               }
-            } // End right hour
-          } // End has task
-        } else {
-          console.log('no token')
-        }
-      })
-      return
-    })
-})
+              // End hour test
+
+              // User defined hour is the right one
+              if (serverHour === userConvertedToServerHour) {
+                const dailyTasks = Object.keys(doc.data().tasks)
+                  .map(e => doc.data().tasks[e])
+                  .filter(task => {
+                    return (
+                      (task.schedule.periodicity === "Weekly" &&
+                        task.schedule.weekly === "Everyday") ||
+                      (task.schedule.periodicity === "On specific days" &&
+                        task.schedule.specificDays.find(v => {
+                          return (
+                            v === getStringFromIsoDay(getISODay(Date.now()))
+                          );
+                        })) ||
+                      (task.schedule.periodicity === "Once" &&
+                        task.schedule.once === "single")
+                    );
+                  })
+                  .filter(task => {
+                    return task.checked === false;
+                  });
+
+                if (dailyTasks.length) {
+                  // Send notification
+                  console.log("user has daily tasks");
+                  var plural = "";
+                  var countDailyTasks = dailyTasks.length;
+                  if (dailyTasks.length > 1) {
+                    plural = "s";
+                  } else {
+                    plural = "";
+                  }
+
+                  // Admin SDK
+                  let message = {
+                    data: {
+                      title: "Weekx",
+                      body: `You have ${countDailyTasks} task${plural} today!'`
+                    },
+                    token: doc.data().profile.notifications.token,
+                    webpush: {
+                      headers: {
+                        TTL: userPushTTL
+                      },
+                      notification: {
+                        title: "Weekx",
+                        body: `You have ${countDailyTasks} task${plural} today!'`,
+                        icon:
+                          "https://app.weekx.xyz/img/icons/android-chrome-512x512.png",
+                        click_action: "https://weekx.netlify.com",
+                        badge:
+                          "https://app.weekx.xyz/img/icons/notification-badge.png"
+                      },
+                      fcm_options: {
+                        link: "https://app.weekx.xyz/"
+                      }
+                    }
+                  };
+                  admin.messaging().send(message);
+                }
+              } // End right hour
+            } // End has task
+          } else {
+            console.log("no token");
+          }
+        });
+        return;
+      });
+  });
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,18 +2,13 @@
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
 
-// self.addEventListener('notificationclick', function (event) {
-//   event.notification.close()
-//   event.waitUntil(self.clients.openWindow('https://weekx.netlify.com'))
-// })
-
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '545270095530'
+  messagingSenderId: '545270095530'
 })
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -21,17 +16,22 @@ firebase.initializeApp({
 const messaging = firebase.messaging()
 
 messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload)
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  )
   // Customize notification here
   var notificationTitle = 'Weekx'
   var notificationOptions = {
     body: payload.data.body,
-    icon: 'https://weekx.netlify.com/img/icons/android-chrome-512x512.png',
-    click_action: 'https://weekx.netlify.com',
-    badge: 'https://weekx.netlify.com/img/icons/notification-badge.png',
+    icon: 'https://app.weekx.xyz/img/icons/android-chrome-512x512.png',
+    click_action: 'https://app.weekx.xyz/',
+    badge: 'https://app.weekx.xyz/img/icons/notification-badge.png',
     silent: true
   }
 
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions)
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  )
 })
